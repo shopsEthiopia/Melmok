8675307d8c4ceb58c048dfc5b5aecd711785c10b
shopsEthiopia <buicodesmk@outlook.com>
2025-05-09 19:09:20 +0300

    Update fillout.js

--- shop/fillout.js
+++ shop/fillout.js
@@ -1,2 +1,12 @@
 let buy1 = document.getElementById("1");
 let buy2 = document.getElementById("2");
+let form = null;
+let productName = null;
+buy1.addEventListener('click' , function(){
+  form = document.createElement('div');
+  form.classList.add('purchase');
+  productName = document.createElement('p');
+  productName.innerHTML = 'product1';
+  form.appennd(productName);
+  document.body.append(form);
+});
